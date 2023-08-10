73b7220aa31bda28ff59bc0c1808f44cea79f280
unknown <viniciusdelimaalex@gmail.com>
2023-08-10 10:45:53 -0300

    inicializando o servidor na porta 8000

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # interfaces_web_TA
 projetos realizados na aula de interfaces web
+
+
+# CLONAR O PROJETO
+
+## GIT CLONE https://github.com/Aloloks/interfaces_web_TA.git
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response)=>{
+    response.send("Olá mundo")
+})
+
+app.listen(8000, ()=>{
+    console.log("Abrindo servidor opsy")
+})
